ea1daf366f0754040ca52efc461b2070d90cf21f
alecmanabat <alecmanabat1@gmail.com>
2016-04-24 17:31:27 -0400

    Player Jumping + Button

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float jumpVelocity = 10;
+
+    Rigidbody2D myBody;
+    public bool isGrounded = false;
+
+    void Start()
+    {
+        myBody = this.GetComponent<Rigidbody2D>();
+       // tagGround = GameObject.Find(this.name + "/tag_ground").transform;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+
+
+    void FixedUpdate()
+    {
+        //isGrounded = Physics2D.Linecast(myTrans.position, -Vector2.up, .000000003f);
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            myBody.velocity += jumpVelocity * Vector2.up;
+            isGrounded = false;
+        }
+    }
+}
